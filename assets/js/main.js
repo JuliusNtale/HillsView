7fe0918d39ecbcdb0d7ffda8026af9153aa0fbb3
NTALE <juliusntale30@gmail.com>
2024-06-16 08:23:23 +0300

    Adding Dark Theme  Changes to be committed: 	modified:   assets/css/styles.css 	modified:   assets/js/main.js 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,6 +125,35 @@ window.addEventListener('scroll', scrollActive)
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-light'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
@@ -436,7 +465,7 @@ const nextState = () => {
 setInterval(() => {
   setState(emotions[nextState()]);
 }, 1500);
-
+   
 
 gsap.from(".box__medium", { opacity: 0, duration: 1.5, delay: 1.3, y: -50 });
 gsap.from(".box__small", { opacity: -1, duration: 1.5, delay: 1.4, y: -50 });
